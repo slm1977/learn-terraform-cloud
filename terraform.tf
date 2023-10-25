69a05c88c081d85db9a7408034ae156f6ce06ba0
Stefano Leone Monni <stefano.monni@crs4.it>
2023-10-25 16:20:29 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+ /*
   cloud {
-    organization = "organization-name"
+    organization = "organization_tutorial"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
